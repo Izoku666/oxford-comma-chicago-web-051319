56661c5fbd25d346495055d6e7b85962cd239177
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 20:09:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
   elsif array.length == 2
-    return "#{array[0]} and #{array[2]}" # add an "and" here
+    return "#{array[0]} and #{array[1]}" # add an "and" here
   else
     index = 0
     for i in array
