96caebe8bec1df30328b201047463991c7d70038
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 20:12:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,16 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}" # add an "and" here
   else
     index = 0
+    full_str = ""
     for i in array
+      str = array[index]
+      if(index == array.length - 1)
+        str = "and " + str
+        full_str += str
+      else
+        str += ", "
+      end
+        
     end
     #do a think for adding commas here
   end
