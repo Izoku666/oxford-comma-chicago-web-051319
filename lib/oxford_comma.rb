8f448bba055407d75ba7bb85dd922f12e5e3ff24
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 20:09:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
   elsif array.length == 2
-    return "" # add an "and" here
+    return "#{array[0]} and #{array[2]}" # add an "and" here
   else
     #do a think for adding commas here
   end
