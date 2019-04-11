3e637172ae9078393b9019ec19c81468bc102ebf
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 20:09:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,9 @@ def oxford_comma(array)
   elsif array.length == 2
     return "#{array[0]} and #{array[2]}" # add an "and" here
   else
+    index = 0
+    for i in array
+    end
     #do a think for adding commas here
   end
 end
